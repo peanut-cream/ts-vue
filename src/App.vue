e4ae2fe5a050d7c435f8d471816d9b08858bc586
walker <walker.wu@tctchina.com.cn>
2020-01-07 18:47:36 +0800

    简单vue结合typescript

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
+      <router-link to="/abc">我自己写的组件</router-link>
     </div>
     <router-view />
   </div>
@@ -19,7 +20,6 @@
 
 #nav {
   padding: 30px;
-
   a {
     font-weight: bold;
     color: #2c3e50;
---
+++ src/views/abc.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    我是abc组件
+    <p>{{ getvalue }}</p>
+    <mycom :name="jim" :age="age" @pass="got"></mycom>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import mycom from "../components/mycom.vue";
+@Component({
+  components: {
+    mycom
+  }
+})
+export default class abc extends Vue {
+  jim: string = "jack";
+  age: number = 11;
+  getvalue: any = "";
+  got(name: any) {
+    this.getvalue = name;
+  }
+}
+</script>

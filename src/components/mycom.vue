5d759d27f8a5a991a27b301b2ba262f61530941f
walker <walker.wu@tctchina.com.cn>
2020-01-08 11:13:21 +0800

    新增prettier配置，自动修复缩进为4

--- src/components/mycom.vue
+++ src/components/mycom.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="box">
-    <div>
-      {{ name + "的年龄" + myage }}
-      <p>有一个{{ num }}岁的哥哥</p>
+    <div class="box">
+        <div>
+            {{ name + '的年龄' + myage }}
+            <p>有一个{{ num }}岁的哥哥</p>
+        </div>
+        <div>
+            <button @click="changeAge">点击改变年龄</button>
+        </div>
     </div>
-    <div>
-      <button @click="changeAge">点击改变年龄</button>
-    </div>
-  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
+import {Component, Vue, Prop} from 'vue-property-decorator';
 @Component
 export default class mycom extends Vue {
-  @Prop({ default: "jack" }) name!: string;
-  @Prop({ default: 23 }) age!: number;
-  get num() {
-    return this.myage + 4;
-  }
-  myname: string = this.name;
-  myage: number = this.age;
-  changeAge(): void {
-    this.myage = Math.floor(Math.random() * 100);
-    this.$emit("pass", this.myname + this.myage);
-  }
+    @Prop({default: 'jack'}) name!: string;
+    @Prop({default: 23}) age!: number;
+    get num() {
+        return this.myage + 4;
+    }
+    myname: string = this.name;
+    myage: number = this.age;
+    changeAge(): void {
+        this.myage = Math.floor(Math.random() * 100);
+        this.$emit('pass', this.myname + this.myage);
+    }
 }
 </script>
 
 <style lang="scss">
 .box {
-  color: red;
+    color: red;
 }
 </style>
--- src/views/abc.vue
+++ src/views/abc.vue
@@ -1,25 +1,25 @@
 <template>
-  <div>
-    我是abc组件
-    <p>{{ getvalue }}</p>
-    <mycom :name="jim" :age="age" @pass="got"></mycom>
-  </div>
+    <div>
+        我是abc组件
+        <p>{{ getvalue }}</p>
+        <mycom :name="jim" :age="age" @pass="got"></mycom>
+    </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue, Prop } from "vue-property-decorator";
-import mycom from "../components/mycom.vue";
+import {Component, Vue, Prop} from 'vue-property-decorator';
+import mycom from '../components/mycom.vue';
 @Component({
-  components: {
-    mycom
-  }
+    components: {
+        mycom,
+    },
 })
 export default class abc extends Vue {
-  jim: string = "jack";
-  age: number = 11;
-  getvalue: any = "";
-  got(name: any) {
-    this.getvalue = name;
-  }
+    jim: string = 'jack';
+    age: number = 11;
+    getvalue: any = '';
+    got(name: any) {
+        this.getvalue = name;
+    }
 }
 </script>
